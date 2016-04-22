f81b398d3ff6d456cfd68c18f09867cbb2bcf602
Ioannis Konstadelias <ikonstadel@gmail.com>
2016-04-23 02:24:45 +0300

    Change everything and use Selenium API instead.

--- true_insomniac.py
+++ true_insomniac.py
@@ -1,101 +1,46 @@
-#!/usr/bin/env python
-# encoding: utf-8
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-import sys
-import getopt
-import base64
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
-from HTMLParser import HTMLParser
+LOGIN_URL = "http://www.insomnia.gr/index.php?app=core&module=global&section=login"
+TARGET_URL = "http://www.insomnia.gr/index.php?app=core&module=search&do=user_activity&mid=223995"
+USER = "username"
+PASS = "password"
 
-# create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
+driver = webdriver.PhantomJS()
+driver.get(LOGIN_URL)
+assert "Insomnia" in driver.title
 
-    first_blood = False # indicates when to start counting the posts
+try:
+    user_elem = driver.find_element_by_name("ips_username")
+    pass_elem = driver.find_element_by_name("ips_password")
+except NoSuchElementException as e:
+    print(e)
+    driver.close()
+    exit()
 
-    count_read = 0
-    count_unread = 0
+user_elem.send_keys(USER)
+pass_elem.send_keys(PASS)
 
-    def handle_starttag(self, tag, attrs):
-        """ Overrides handle_starttag:
-            When the <div> tag with class 'ipsLayout_content' is found,
-            set the flag `first_blood` in order to search into <tr> tags.
-            <tr> tags have an attribute class, where there is the word
-            '_recordRow' some others and the word 'unread' if the post is
-            unread. If the post is read, simply there is no 'unread'.
-        """
-        # Scan the <div> tags
-        if tag == u'div' and not self.first_blood:
-            for attr in attrs:
-                if attr[0] == u'class' and attr[1] == u'ipsLayout_content':
-                    self.first_blood = True
-                    break
+try:
+    pass_elem.submit()
+except NoSuchElementException as e:
+    print(e)
+    driver.close()
+    exit()
 
-        # Scan the <tr> tags
-        if tag == 'tr' and self.first_blood:
-            for attr in attrs:
-                if attr[0] == u'class' and u'_recordRow' in attr[1]:
-                    if (u'unread' in attr[1]):
-                        self.count_unread += 1;
-                    else:
-                        self.count_read += 1;
+driver.get(TARGET_URL)
 
-    #def handle_startendtag(self, tag, attrs):
-    #    if (tag == u'img'):
-    #        for attr in attrs:
-    #            if (attr[0] == u'src'):
-    #                print "\t" + str(attr)
+try:
+    unread_elems = driver.find_elements_by_class_name("unread")
+except NoSuchElementException as e:
+    print(e)
+    driver.close()
+    exit()
 
+print("\u2605", len(unread_elems))
 
-def parse_args(argv):
-    """ Parse the arguments from `argv`. They must be a username and
-        a password as the usage says. I unexpected arguments are met,
-        then exit.
-    """
-    username, password = '', ''
-
-    try:
-        opts, args = getopt.getopt(argv,"hu:p:",["username=","password="])
-    except getopt.GetoptError:
-        print 'true_insomniac.py -u <username> -p <password>'
-        sys.exit(2)
-
-    if len(opts) == 0:
-        print 'true_insomniac.py -u <username> -p <password>'
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'true_insomniac.py -u <username> -p <password>'
-            sys.exit()
-        elif opt in ("-u", "--username"):
-            username = arg
-        elif opt in ("-p", "--password"):
-            password = arg
-
-    return username, password
-
-
-def main(argv):
-    url = 'http://www.insomnia.gr/index.php?app=core&module=search&do=user_activity&mid=223995'
-
-    # Parse the arguments
-    username, password = parse_args(argv)
-
-    # Get the contents of the url
-    r = requests.get(url, auth=(username, password)) # send auth unconditionally
-    r.raise_for_status() # raise an exception if the authentication fails
-    user_contents = r.text
-
-    # Parse the contents of the url
-    parser = MyHTMLParser()
-    parser.feed(user_contents);
-
-    # Print the results
-    print "READ   = " + str(parser.count_read)
-    print "UNREAD = " + str(parser.count_unread)
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
-
+driver.close()
